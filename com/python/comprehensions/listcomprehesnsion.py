741f4dd9313795f6b2093e415096044a82cc1274
jnaneshjain <47937157+jnaneshjain@users.noreply.github.com>
2019-04-11 22:50:29 +0530

    List Comprehension

--- com/python/comprehensions/listcomprehesnsion.py
+++ com/python/comprehensions/listcomprehesnsion.py
@@ -1,4 +1,4 @@
-# Python Program To demonstrate List comprehensions in Python
+# Python Program To demonstrate List Comprehensions in Python
 
 # below list contains square of all odd numbers from
 # Range 1 to 10
