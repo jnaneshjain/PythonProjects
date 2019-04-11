9c69a6638f8d3dc74549191aa5fb7788c3ddf2fa
jnaneshjain <jnanesh.jain@arisglobal.com>
2019-04-11 22:49:04 +0530

    Hello

--- com/python/comprehensions/dictionarycomprehension.py
+++ com/python/comprehensions/dictionarycomprehension.py
@@ -1,4 +1,4 @@
-# Python Code to Demonstrate dictionary comprehension
+# Python Code to Demonstrate dictionary comprehensions
 
 # Lists to represent keys and values
 keys = ['a', 'b', 'c', 'd', 'e']
@@ -7,7 +7,7 @@ values = [1, 2, 3, 4, 5]
 # But this line shows dict compreHension here
 myDict = {k: v for (k, v) in zip(keys, values)}
 
-# We can use below too
+# We can use below method too
 # myDict = dict(zip(keys, values))
 
 print(myDict)
