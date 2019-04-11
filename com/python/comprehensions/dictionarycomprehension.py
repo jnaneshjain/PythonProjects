9b5beb78d785f4afbe47344e9d50ce188f87d3f4
jnaneshjain <jnanesh.jain@arisglobal.com>
2019-04-11 21:53:26 +0530

    Commit message

---
+++ com/python/comprehensions/dictionarycomprehension.py
@@ -0,0 +1,13 @@
+# Python code to demonstrate dictionary
+
+# Lists to represent keys and values
+keys = ['a', 'b', 'c', 'd', 'e']
+values = [1, 2, 3, 4, 5]
+
+# but this line shows dict compreHension here
+myDict = {k: v for (k, v) in zip(keys, values)}
+
+# We can use below too
+# myDict = dict(zip(keys, values))
+
+print(myDict)
