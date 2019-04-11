1af7902b8f2d23e250643df7320ab6949b3c4bb6
jnaneshjain <jnanesh.jain@arisglobal.com>
2019-04-11 22:34:12 +0530

    Conflicts resloved

--- inputexercise.py
+++ inputexercise.py
@@ -1,7 +1,7 @@
 weight = int(input('Weight :'))
 unit = input('(L)bs or (K)g: ')
 if unit.upper() == 'L':
-    converted = weight * 0.45
+    converted = weight * 00.45
     print(f"Your are {converted} kilos")
 else:
     converted = weight /0.45
