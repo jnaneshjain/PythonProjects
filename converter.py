da514d65ed884336c2c4ad5f1635fc180f3c3857
jnaneshjain <47937157+jnaneshjain@users.noreply.github.com>
2019-04-11 22:23:43 +0530

    Stash Test

--- converter.py
+++ converter.py
@@ -1,10 +1,10 @@
 
 def lbs_to_kg(weight):
-    return weight * 00.45
+    return weight * 0.45
 
 
 def kg_to_lbs(weight):
-    return weight / 00.45
+    return weight / 0.45
 
 def find_max(numbers):
      max = numbers[0]
