09830a65c244fc4e0534d5174301e9b04acadfb2
jnaneshjain <jnanesh.jain@arisglobal.com>
2019-04-12 12:09:19 +0530

    Package changed

--- com/python/pythoncomprehensions/dictionarycomprehension.py
+++ com/python/pythoncomprehensions/dictionarycomprehension.py
@@ -5,7 +5,7 @@ keys = ['a', 'b', 'c', 'd', 'e']
 values = [1, 2, 3, 4, 5]
 
 # But this line shows dict compreHension here
-myDict = {k: v for (k, v) in zip(keys, values)}
+myDict = {"k: v for (k, v) in zip(keys, values)}
 
 # We can use below method too
 # myDict = dict(zip(keys, values))
